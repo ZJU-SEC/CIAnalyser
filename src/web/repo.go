ff3fb6dd18d6b2cc7179e684e5899e288170fdca
PAN-Ziyue <ziyuepan99@outlook.com>
2022-01-17 22:42:49 +0800

    add user / org parser

--- src/web/repo.go
+++ src/web/repo.go
@@ -9,29 +9,65 @@ import (
 // Crawl the repositories
 func Crawl() {
 	crawlGitstarRepo()
+	crawlGitstarUserOrg()
 }
 
-func crawlGitstarRepo() {
-	c := colly.NewCollector()
+
+func commonCollector() *colly.Collector {
+    c := colly.NewCollector()
 
 	// set random `User-Agent`
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", utils.RandomString())
 	})
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		fmt.Println(link)
-		// TODO parse more
-
-	})
-
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
+
+    return c
+}
+
+
+func crawlGitstarRepo() {
+    c := commonCollector()
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if e.Attr("class") == "list-group-item paginated_item" {
+			crawlGitHubRepo(link)
+		}
+	})
+
 
 	for i := 1; i <= 50; i++ {
 		pageURL := fmt.Sprintf("https://gitstar-ranking.com/repositories?page=%d", i)
 		c.Visit(pageURL)
 	}
 }
+
+func crawlGitstarUserOrg() {
+    c := commonCollector()
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if e.Attr("class") == "list-group-item paginated_item" {
+            fmt.Println(link)
+            // TODO parse user / org
+		}
+    })
+
+    for i := 1; i <= 50; i++ {
+		userURL := fmt.Sprintf("https://gitstar-ranking.com/users?page=%d", i)
+        orgURL := fmt.Sprintf("https://gitstar-ranking.com/organizations?page=%d", i)
+		c.Visit(userURL)
+        c.Visit(orgURL)
+    }
+}
+
+func crawlGitHubRepo(href string) {
+    // TODO parse repo
+    fmt.Println(href)
+}
+
+
