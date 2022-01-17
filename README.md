7b07f0d1fb19d4476fe4a615812725effefd8a09
PAN-Ziyue <ziyuepan99@outlook.com>
2022-01-17 20:22:42 +0800

    add docker start instructions

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# CIHunter
+# :robot: CIHunter
+
+[![Build](https://github.com/ZJU-SEC/CIHunter/actions/workflows/build.yml/badge.svg)](https://github.com/ZJU-SEC/CIHunter/actions/workflows/build.yml)
+
+##  Prerequisite
+
+- Golang
+- PostgreSQL
+
+### :bulb: Dockerized PostgreSQL
+
+To run a dockerized PostgreSQL, check [this](https://hub.docker.com/_/postgres).
+
+Start a postgres container:
+
+```bash
+$ docker run \
+  --name postgres -d \
+  -e POSTGRES_USER=ZJU-SEC \
+  -e POSTGRES_PASSWORD=<YOUR DB PASSWORD> \
+  -e POSTGRES_DB=CIHunter 
+  -p 5432:5432 postgres
+```
+
+
+## :hammer_and_wrench: Build
+
+```bash
+$ go build CIHunter
+```
--- src/web/repo.go
+++ src/web/repo.go
@@ -2,14 +2,36 @@ package crawler
 
 import (
 	"CIHunter/src/utils"
+	"fmt"
 	"github.com/gocolly/colly"
 )
 
 // Crawl the repositories
 func Crawl() {
+	crawlGitstarRepo()
+}
+
+func crawlGitstarRepo() {
 	c := colly.NewCollector()
+
+	// set random `User-Agent`
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", utils.RandomString())
 	})
 
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Println(link)
+		// TODO parse more
+
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	for i := 1; i <= 50; i++ {
+		pageURL := fmt.Sprintf("https://gitstar-ranking.com/repositories?page=%d", i)
+		c.Visit(pageURL)
+	}
 }
